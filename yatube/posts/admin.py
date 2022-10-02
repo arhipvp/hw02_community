d5860225d913ff893fc3e359a79eb9a1b736311c
Архипов Владимир <arhipvp@gmail.com>
2022-10-02 18:08:03 +0300

    Поправил по flake8

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,9 +1,7 @@
-from calendar import c
 from django.contrib import admin
-from pkg_resources import empty_provider
 from .models import Post, Group
 
-# Register your models here.
+
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         'id',
@@ -16,6 +14,7 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('pub_date', 'author_id')
     empty_value_display = '-пусто-'
     list_editable = ('group',)
-    
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Group, Post
 
-# Create your views here.
+
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
     template = 'posts/index.html'
@@ -10,6 +10,7 @@ def index(request):
         'posts': posts,
     }
     return render(request, template, context=context)
+
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
@@ -21,4 +22,3 @@ def group_posts(request, slug):
         'posts': posts,
     }
     return render(request, template, context=context)
-    
