ca512346f728230ca9f11bcdb46023bdad627c0e
Архипов Владимир <arhipvp@gmail.com>
2022-10-02 19:28:12 +0300

    -

---
+++ yatube/yatube/posts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Group, Post
+
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    template = 'posts/index.html'
+    context = {
+        'title': 'Последние обновления на сайте',
+        'posts': posts,
+    }
+    return render(request, template, context=context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    template = 'posts/group_list.html'
+    title = f'Записи сообщества {group.__str__()}'
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, template, context=context)
