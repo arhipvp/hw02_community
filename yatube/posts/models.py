1a64c1ca51d4daa42dce0dc9ef0245c36493ca40
arhipvp <112897986+arhipvp@users.noreply.github.com>
2022-10-02 19:01:43 +0300

    Update models.py
    -

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,7 +5,7 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.TextField()
+    title = models.TextField(max_length=200)
     slug = models.SlugField()
     description = models.TextField()
 
